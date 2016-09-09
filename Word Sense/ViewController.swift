086d90f91c0d42981601b9630e21ed6d5cec6a5e
adamgrav <gravelyadam@gmail.com>
2016-09-09 13:12:25 -0400

    Initial Commit

---
+++ Word Sense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Sense
+//
+//  Created by Adam Gravely on 9/9/16.
+//  Copyright © 2016 Spacebar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
