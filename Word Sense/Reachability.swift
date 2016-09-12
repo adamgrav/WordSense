c67b1ae1b6fe487606d7cc517a0a9175026c0383
adamgrav <gravelyadam@gmail.com>
2016-09-11 22:21:08 -0400

    First version with Google Cloud Vision API

---
+++ Word Sense/Reachability.swift
@@ -0,0 +1,30 @@
+//
+//  Reachability.swift
+//  Word Sense
+//
+//  Created by Adam Gravely on 9/7/16.
+//  Copyright © 2016 Spacebar. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
--- Word Sense/ViewController.swift
+++ Word Sense/ViewController.swift
@@ -8,18 +8,240 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
 
-    override func viewDidLoad() {
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l > r
+    default:
+        return rhs < lhs
+    }
+}
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var textResults: UITextView!
+    @IBOutlet weak var wordCount: UILabel!
+    @IBOutlet weak var textFoundLabel: UILabel!
+    
+    
+    
+    var API_KEY = "AIzaSyBo42-xNuufRV_6D39SjL1kTDaJokn2Znk"
+    
+    @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func openCamera(_ sender: AnyObject) {
+        imageView.isHidden = false
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage // You could optionally display the image here by setting imageView.image = pickedImage
+            spinner.startAnimating()
+            textResults.isHidden = true
+            wordCount.isHidden = true
+            textFoundLabel.isHidden = true
+            
+            if(!Reachability.isConnectedToNetwork()) {
+                spinner.stopAnimating()
+                //let alertController = UIAlertController(title: "No internet connection", message: "Requires internet connection to analyze image", preferredStyle: UIAlertControllerStyle.alert)
+                //alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                //self.present(alertController, animated: true, completion: nil)
+                self.textResults.isHidden = false
+                self.wordCount.isHidden = false
+                self.textFoundLabel.isHidden = false
+                self.wordCount.text = "No Connection"
+                self.textFoundLabel.text = "..."
+                self.textResults.text = "Image Sensing Requires Internet Connection...\n\nTry connecting to a WiFi signal"
+            }
+            else {
+                // Base64 encode the image and create the request
+                let binaryImageData = base64EncodeImage(pickedImage)
+                createRequest(binaryImageData)
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+        imageView.isHidden = true
+    }
+    
+    func resizeImage(_ imageSize: CGSize, image: UIImage) -> Data {
+        UIGraphicsBeginImageContext(imageSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let resizedImage = UIImagePNGRepresentation(newImage!)
+        UIGraphicsEndImageContext()
+        return resizedImage!
+    }
+    
+    func base64EncodeImage(_ image: UIImage) -> String {
+        var imagedata = UIImagePNGRepresentation(image)
+        
+        // Resize the image if it exceeds the 2MB API limit
+        if ((imagedata?.count)! > 2097152) {
+            let oldSize: CGSize = image.size
+            let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
+            imagedata = resizeImage(newSize, image: image)
+        }
+        
+        return imagedata!.base64EncodedString(options: .endLineWithCarriageReturn)
+    }
+    
+    func createRequest(_ imageData: String) {
+        // Create our request URL
+        let request = NSMutableURLRequest(
+            url: URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(API_KEY)")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(
+            Bundle.main.bundleIdentifier ?? "",
+            forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+        
+        // Build our API request
+        let jsonRequest: [String: Any] = [
+            "requests": [
+                "image": [
+                    "content": imageData
+                ],
+                "features": [
+                    [
+                        "type": "TEXT_DETECTION"
+                        
+                    ]
+                ]
+            ]
+        ]
+        
+        // Serialize the JSON
+        request.httpBody = try! JSONSerialization.data(withJSONObject: jsonRequest, options: [])
+        
+        // Run the request on a background thread
+        DispatchQueue.global(priority: DispatchQueue.GlobalQueuePriority.default).async(execute: {
+            self.runRequestOnBackgroundThread(request as URLRequest)
+        });
+        
+    }
+    
+    func runRequestOnBackgroundThread(_ request: URLRequest) {
+        
+        let session = URLSession.shared
+        
+        // run the request
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            data, response, error in
+            self.analyzeResults(data!)
+        })
+        task.resume()
+    }
+    
+    func analyzeResults(_ dataToParse: Data) {
+        
+        // Update UI on the main thread
+        DispatchQueue.main.async(execute: {
+            
+            
+            // Use SwiftyJSON to parse results
+            let json = JSON(data: dataToParse)
+            let errorObj: JSON = json["error"]
+            
+            self.spinner.stopAnimating()
+            self.imageView.isHidden = true
+            //
+            self.textResults.isHidden = false
+            self.wordCount.isHidden = false
+            self.textFoundLabel.isHidden = false
+            //
+            
+            // Check for errors
+            if (errorObj.dictionaryValue != [:]) {
+                self.wordCount.isHidden = true
+                self.textResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
+            }
+            else {
+                // Parse the response
+                print(json)
+                let responses: JSON = json["responses"][0]
+                
+                //Get text annotations
+                let textAnnotations: JSON = responses["textAnnotations"]
+                let numWords: Int = textAnnotations.count
+                var words: Array<String> = []
+                
+                if numWords > 0 {
+                    var textResultsText:String = ""
+                    for index in 1..<numWords {
+                        let word = textAnnotations[index]["description"].stringValue
+                        words.append(word)
+                    }
+                    for word in words {
+                        //if it's not the last item add a comma
+                        if words[words.count - 1] != word {
+                            textResultsText += "\(word) "
+                        } else {
+                            textResultsText += "\(word)"
+                        }
+                    }
+                    self.textResults.text = textResultsText
+                    self.wordCount.text = "\(numWords-1) Words"
+                } else {
+                    self.textResults.text = "No text found"
+                    self.wordCount.text = "x  -  x"
+                    
+                }
+            }
+        })
+        
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        textResults.isHidden = true
+        wordCount.isHidden = true
+        textFoundLabel.isHidden = true
+        spinner.hidesWhenStopped = true
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
